4da54c6091f3480c0f5bda3896e3d0187f3504d1
hk-p114514 <hukada.gogotea@gmail.com>
2021-08-30 07:48:47 +0900

    縦スクロールを調整

--- version_1.0/src/classes/Background.ts
+++ version_1.0/src/classes/Background.ts
@@ -1,11 +1,11 @@
-import { canvas_h, canvas_w } from '../init/variables';
+import { canvas_h, canvas_w, screen_h, screen_w } from '../init/variables';
 
 class Background {
 	private image = new Image();
 	private width: number = canvas_w;
 	private height: number = canvas_h;
 	private x: number = this.image.width / 2;
-	private y: number = 0;
+	private y: number = this.image.height;
 	private count: number = 0;
 	constructor(src: string) {
 		this.image.setAttribute('src', src);
@@ -14,11 +14,6 @@ class Background {
 	draw(ctx: CanvasRenderingContext2D) {
 		const width = this.width;
 		const height = this.height;
-		// ctx.drawImage(this.image, 0, 0, width, height, 0, 0, width, height);
-		const pattern = ctx.createPattern(this.image, 'repeat');
-		if (pattern) {
-			ctx.fillStyle = pattern;
-		}
 		ctx.drawImage(
 			this.image,
 			this.x,
