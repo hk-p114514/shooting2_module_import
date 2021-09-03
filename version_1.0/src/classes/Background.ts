d344211a105a3afadc27403b182c51401347f2e4
hk-p114514 <hukada.gogotea@gmail.com>
2021-09-03 18:04:36 +0900

    背景のリピートの動作確認

--- version_1.0/src/classes/Background.ts
+++ version_1.0/src/classes/Background.ts
@@ -1,14 +1,28 @@
-import { canvas_h, canvas_w, screen_h, screen_w } from '../init/variables';
+import {
+	canvas_h,
+	canvas_w,
+	field_h,
+	player,
+	screen_h,
+	screen_w,
+} from '../init/variables';
 
 type Point = { x: number; y: number };
 
 class Background {
 	private imageBottom = new Image();
 	private imageTop = new Image();
-	private width: number = canvas_w;
-	private height: number = canvas_w + screen_h;
-	private bottom: Point = { x: this.imageBottom.width / 2, y: 0 };
 	private count: number = 0;
+
+	private bottom: Point = {
+		x: this.imageBottom.width / 2,
+		y: 0,
+	};
+
+	private top: Point = {
+		x: this.imageTop.width / 2,
+		y: -screen_h,
+	};
 
 	constructor(srcBottom: string, srcTop: string) {
 		this.imageBottom.setAttribute('src', srcBottom);
@@ -16,32 +30,44 @@ class Background {
 	}
 
 	draw(ctx: CanvasRenderingContext2D) {
-		const width = this.width;
-		const height = this.height;
-		ctx.drawImage(
-			this.imageBottom,
-			this.bottom.x,
-			this.bottom.y,
-			width,
-			height,
-			0,
-			0,
-			width,
-			height,
-		);
+		this.drawBg(ctx, this.imageBottom, this.bottom);
+		this.drawBg(ctx, this.imageTop, this.top);
 	}
 
 	update() {
 		this.count++;
 
-		console.log(`y : ${this.bottom.y}`);
 		this.scroll(this.bottom);
+		this.scroll(this.top);
 	}
 
+	drawBg = (
+		ctx: CanvasRenderingContext2D,
+		image: HTMLImageElement,
+		p: Point,
+	) => {
+		const width = screen_w / 2;
+		ctx.drawImage(
+			image,
+			width,
+			0,
+			canvas_w,
+			canvas_h,
+			p.x,
+			p.y,
+			canvas_w,
+			canvas_h,
+		);
+	};
+
 	scroll = (img: Point) => {
-		if (this.count % 5 === 0) {
-			this.count = 0;
-			img.y--;
+		// if (this.count % 5 === 0) {
+		this.count = 0;
+		img.y++;
+		// }
+
+		if (img.y > canvas_h) {
+			img.y = screen_h;
 		}
 	};
 }
