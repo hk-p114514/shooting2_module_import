7743c3adb673b0f2f76fa7d71a148b5c5cf30d1f
hk-p114514 <hukada.gogotea@gmail.com>
2021-09-29 18:11:33 +0900

    群青色のヒヨコを低空飛行に

--- version_1.0/src/functions/enemyFunctions/enemyMoveBlue/lvl1.ts
+++ version_1.0/src/functions/enemyFunctions/enemyMoveBlue/lvl1.ts
@@ -1,5 +1,5 @@
 import { Enemy } from '../../../classes/Enemy';
-import { player } from '../../../init/variables';
+import { field_h, player } from '../../../init/variables';
 import { changeSprite } from '../changeSprite';
 import { enemyAfterAttack } from '../enemyAfterAttack';
 import { enemyBeforeAttack } from '../enemyBeforeAttack';
@@ -10,9 +10,9 @@ const lvl1 = (enemy: Enemy) => {
 	const vxMax = 120;
 	const approachX = 400;
 	const breakOutX = 30;
-
 	// 敵キャラが降下可能な最大限の高さ(低さ)
-	const limitY = -200;
+	const limitY = -field_h / 2;
+
 	if (!enemy.flag) {
 		enemyBeforeAttack(enemy, player, acceleration, vxMax);
 	} else {
@@ -27,16 +27,16 @@ const lvl1 = (enemy: Enemy) => {
 		);
 	}
 
-	if (enemy.isAttackable(player.y, 500, false) && !enemy.flag) {
+	if (enemy.isAttackable(player.y, 300, false) && !enemy.flag) {
 		// プレイヤーと自分の距離が300以内で、自分が攻撃していなかったら
 		enemy.flag = true;
 		//enemyBulletを呼び出した回数分、攻撃する
 		for (let i = 0; i < 50; i++) {
-			enemyBullet(enemy, 500, { gap: i, moveCount: 4 });
+			enemyBullet(enemy, 250, { gap: i, moveCount: 2 });
 		}
 	}
 
-	if (enemy.flag && enemy.vy > limitY) {
+	if (enemy.flag && enemy.y > limitY) {
 		// もう攻撃した & 自分のy座標がlimitYより大きい時
 		enemy.accelerationY(-breakOutX);
 	}
