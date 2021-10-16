2438eabaa5587288dc869a4b842d331635c42873
hk-p114514 <hukada.gogotea@gmail.com>
2021-10-16 15:17:39 +0900

    BGM有効化

--- version_1.0/src/functions/waves/waves.ts
+++ version_1.0/src/functions/waves/waves.ts
@@ -1,5 +1,5 @@
 import { lvl1Waves } from './lvl1Waves';
-import { lvl2Waves } from './lvl2Waves';
+// import { lvl2Waves } from './lvl2Waves';
 import { lvl3Waves } from './lvl3Waves';
 import { testWave } from './test/testWave';
 
